bd19a121abcd7cda3ca8b70004711aec6ac1a627
PabloA-Figueroa <apfigueroa3@utpl.edu.ec>
2023-11-17 11:03:48 -0500

    primer Commit

---
+++ camaraComercio/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from camaraComercio.models import camarita
+
+admin.site.register(camarita)
+
+
+# Register your models here.
---
+++ camaraComercio/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class camarita(models.Model):
+    campo_1 = models.CharField(max_length=100)
+    campo_2 = models.CharField(max_length=100)
+# Create your models here.
